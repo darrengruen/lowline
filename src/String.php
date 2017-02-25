11d51b0310659c9997d9d84524756a1965cb7dcd
Darren Green <darrensgreen@gmail.com>
2017-02-25 15:51:56 -0600

    Removed old test files
    Removed old test files

--- src/String.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace dgruen\LowLine;
-
-use funcs\StringTrait as Base;
-
-class String
-{
-    use Base;
-}
--- tests/StringTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace dgruen\Tests\LowLine;
-
-use dgruen\Tests\LowLine\funcs\StringTraitTest as BaseTest;
-class StringTest extends BaseTest
-{
-    // This is an empty test class
-}
