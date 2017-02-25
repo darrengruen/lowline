c01a6a778738e8334cff75eb44647ad14abdc437
Darren Green <darrensgreen@gmail.com>
2017-02-25 14:34:01 -0600

    Add isCallable method
    Add isCallable method

--- src/funcs/TestTrait.php
+++ src/funcs/TestTrait.php
@@ -3,13 +3,25 @@ namespace dgruen\LowLine\funcs;
 
 trait TestTrait
 {
+
     /**
-    * Tests if $testCase is a numeric value
-    *
-    * @param  [mixed]  $testCase The variable to check
-    * @return boolean           Returns true if $testCase is numeric, false
-    * otherwise
-    */
+     * Test if a value is a php callable
+     *
+     * @param  mixed   $testCase Value to test
+     * @return boolean           Returns true if $testCase is a callable, false otherwise
+     */
+    public function isCallable($testCase = null)
+    {
+        return is_callable($testCase);
+    }
+
+    /**
+     * Tests if $testCase is a numeric value
+     *
+     * @param  [mixed]  $testCase The variable to check
+     * @return boolean           Returns true if $testCase is numeric, false
+     * otherwise
+     */
     public function isNumeric($testCase = null)
     {
         return is_numeric($testCase);
@@ -24,5 +36,16 @@ trait TestTrait
     public function isObject($testCase = null)
     {
         return is_object($testCase);
+    }
+
+    /**
+     * Test if $testCase is a string
+     *
+     * @param  mixed  $testCase The testcase
+     * @return boolean          Returns true if $testCase is a string, false otherwise
+     */
+    public function isString($testCase = null)
+    {
+        return is_string($testCase);
     }
 }
